3c88555a3a6c0721151899136261ed017d8bb7ad
CJ Carlson <cjcarlson@berkeley.edu>
2019-05-02 15:44:56 -0400

    Some more documentation, everything should be indexed

---
+++ rangeshifts-package.R
@@ -0,0 +1,15 @@
+#' @title Range Shifs from Historical Data
+#'
+#' @description This package streamlines the process of tracking range shifts based on historical range data, using a combination of interval comparison methods and regressions. As more methods are developed I hope this will become crowdsourced and more robust, but right now the goal is to mostly cover methods used by studies covered in Chen et al. (2011)
+#'
+#'
+#' @name rangeshifts-package
+#' @aliases crangeshifts-package rangeshifts
+#' @docType package
+#' @author Colin Carlson (\email{cjc322@georgetown.edu})
+#'
+#' @import tidyverse
+#'
+#'
+#'
+NULL
